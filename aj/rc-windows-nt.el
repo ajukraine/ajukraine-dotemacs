486606e5e5a0a78faf2b6b9038458f1b1708f7c5
Bohdan Makohin <ajukraine@gmail.com>
2012-07-30 10:47:54 +0300

    WindowsNT: Use ANSI colors when in Shell

--- aj/rc-windows-nt.el
+++ aj/rc-windows-nt.el
@@ -2,6 +2,9 @@
 (add-site-lisp-dir "ntemacs-cygwin")
 
 (setq exec-path (cons"c:/cygwin/bin" exec-path))
-
 (require 'ntemacs-cygwin)
+
+(require 'ansi-color)
+(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+
 (provide 'rc-windows-nt)
